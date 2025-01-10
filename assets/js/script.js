22d9f694de0762b382105e103d91adef87366657
Lucas Merini Flores <merini.lucas@gmail.com>
2025-01-10 19:12:20 -0300

    Atualizado certificados e projetos 10-01-2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,53 @@ designFilter.addEventListener('click', () => {
         }
     });
 });
+
+
+// Imagem 3D
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.project-buttons button');
+    
+    buttons.forEach(button => {
+        button.addEventListener('mousemove', function(e) {
+            const width = button.offsetWidth;
+            const height = button.offsetHeight;
+            const offsetX = e.offsetX;
+            const offsetY = e.offsetY;
+            
+            const moveX = (offsetX / width) * 30 - 15; 
+            const moveY = (offsetY / height) * 30 - 15; 
+            
+            button.style.transform = `perspective(1000px) rotateX(${moveY}deg) rotateY(${moveX}deg)`;
+        });
+
+        button.addEventListener('mouseleave', function() {
+            button.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+        });
+    });
+});
+
+
+// Imagem Certificado
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('certificateModal');
+    const modalImage = document.getElementById('modalImage');
+    const closeBtn = document.querySelector('.modal .close');
+    const certificates = document.querySelectorAll('.certificate img');
+
+    certificates.forEach(certificate => {
+        certificate.addEventListener('click', () => {
+            modal.style.display = 'flex';
+            modalImage.src = certificate.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
